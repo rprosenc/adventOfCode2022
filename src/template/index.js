08d41aea555426340b955eab70ae3e43bfc9e4d4
Robert Prosenc <976156+rprosenc@users.noreply.github.com>
2022-12-02 21:22:36 +0100

    reniced template

--- src/template/index.js
+++ src/template/index.js
@@ -3,36 +3,36 @@ import run from "aocrunner";
 const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput);
+    const input = parseInput(rawInput);
 
-  return;
+    return;
 };
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput);
+    const input = parseInput(rawInput);
 
-  return;
+    return;
 };
 
 run({
-  part1: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: false,
+    part1: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
 });
