a05b6341605184c71d78e77000a9bb9b63365a9b
Robert Prosenc <976156+rprosenc@users.noreply.github.com>
2022-12-02 21:23:07 +0100

    day 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.424ms
-Time part 2: 0.507ms
-Both parts: 0.9308719999999999ms
+Time part 1: 0.387ms
+Time part 2: 0.385ms
+Both parts: 0.7720009999999999ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.296ms
+Time part 2: 0.448ms
+Both parts: 0.744034ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 0.931ms
+Total stars: 4/50
+Total time: 1.516ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.js
@@ -0,0 +1,67 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput.split('\n');
+
+const names = ['Rock', 'Paper', 'Scissors'];
+
+const score = { 
+    'A X': 1 + 3,
+    'A Y': 2 + 6,
+    'A Z': 3 + 0,
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+    'C X': 1 + 6,
+    'C Y': 2 + 0,
+    'C Z': 3 + 3,
+}
+
+const part1 = (rawInput) => {
+    const input = parseInput(rawInput);
+    return input.reduce((acc,val) => acc + score[val], 0);
+};
+
+const repair = {
+    'A X': 'A Z',
+    'A Y': 'A X',
+    'A Z': 'A Y',
+    'B X': 'B X',
+    'B Y': 'B Y',
+    'B Z': 'B Z',
+    'C X': 'C Y',
+    'C Y': 'C Z',
+    'C Z': 'C X',
+    }
+
+const part2 = (rawInput) => {
+    const input = parseInput(rawInput);
+
+    return input.reduce((acc,val) => acc + score[repair[val]], 0);
+};
+
+run({
+    part1: {
+        tests: [
+            {
+               input: `A Y
+B X
+C Z`,
+              expected: 15,
+            },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            {
+              input: `A Y
+B X
+C Z`,
+              expected: 12,
+            },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
+});
