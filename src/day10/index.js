3b638a9f5a4b42b348aa677581f558218cd9f57f
Robert Prosenc <976156+rprosenc@users.noreply.github.com>
2022-12-10 10:23:13 +0100

    added day 10/2

--- src/day10/index.js
+++ src/day10/index.js
@@ -34,11 +34,6 @@ const part1 = (rawInput) => {
             processingQueue.unshift(Number(val));
         }
 
-        if (cycle>=180) {
-            //console.log({cycle, cmd, val, regX})
-        }
-
-
         cycle++;
     }
 
@@ -48,7 +43,57 @@ const part1 = (rawInput) => {
 const part2 = (rawInput) => {
     const input = parseInput(rawInput);
 
-    return;
+    let regX = 1;
+    let cycle = 1;
+    const processingQueue = [];
+    const sigStrengths = []
+    let crt = '';
+    let cmd, val, foo, line, pos, sprite;
+    while(processingQueue.length || input.length) {
+        cmd = '    '; val = 0;
+
+        // print at screen
+        pos = crt.length % 40;
+        sprite = [regX-1, regX, regX+1];
+        crt += (sprite.includes(pos)) ? '█' : ' ';
+        //console.log(cycle, regX, crt);
+        
+
+        if (processingQueue.length > 0) {
+            regX += processingQueue.pop();
+        } else if (input.length) {
+            foo = input.shift().split(' ');
+            cmd = foo[0];
+            if (foo.length>1) {
+                val = Number(foo[1])
+            }
+        }
+        if (cmd === 'noop') {
+            // do nothing
+        }
+        if (cmd === 'addx') {
+            processingQueue.unshift(Number(val));
+        }
+
+        if (cycle>=40) {
+            //break;
+        }
+
+        cycle++;
+    }
+
+    const buffer = [];
+    for(let i=0; i<=239; i++) {
+        if (i%40 === 0) {
+            buffer.push('');
+        }
+
+        buffer[buffer.length-1] += crt[i];
+    }
+
+    console.log(buffer);
+
+    return 'RKPJBPLA'
 };
 
 run({
@@ -212,8 +257,154 @@ noop
         tests: [
             {
                 input: `
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
 `,
-                expected: "",
+                expected: 'test1',
             },
         ],
         solution: part2,
